feae63071108f1c9e63c80fefe9eae3da2efd0b8
trumully <truman.mulholland@gmail.com>
2024-05-09 13:21:39 +1200

    test: rewrite tests

--- src/Tests/FlatSharpEndToEndTests/ToStringTests/ToStringTests.cs
+++ src/Tests/FlatSharpEndToEndTests/ToStringTests/ToStringTests.cs
@@ -1,106 +1,24 @@
 namespace FlatSharpEndToEndTests.ToStringTests;
 
 [TestClass]
-public class ToStringTests
+public class ToStringTestCases()
 {
     [TestMethod]
     [DynamicData(nameof(DynamicDataHelper.DeserializationModes), typeof(DynamicDataHelper))]
-    public void Table_ToString(FlatBufferDeserializationOption option)
+    public void All_ToString(FlatBufferDeserializationOption option)
     {
-        byte[] table = new Table
-        {
-            A = 1,
-            B = 2,
-            C = 3,
-            D = 4,
-        }.AllocateAndSerialize();
-        Table parsed = Table.Serializer.Parse(table, option);
-        
-        Assert.AreEqual("Table { A = 1, B = 2, C = 3, D = 4 }", parsed.ToString());
-    }
-
-    [TestMethod]
-    [DynamicData(nameof(DynamicDataHelper.DeserializationModes), typeof(DynamicDataHelper))]
-    public void EmptyTable_ToString(FlatBufferDeserializationOption option)
-    {
-        byte[] emptyTable = new EmptyTable().AllocateAndSerialize();
-        EmptyTable parsed = EmptyTable.Serializer.Parse(emptyTable, option);
-
-        Assert.AreEqual("EmptyTable { }", parsed.ToString());
-    }
-
-    [TestMethod]
-    [DynamicData(nameof(DynamicDataHelper.DeserializationModes), typeof(DynamicDataHelper))]
-    public void Struct_ToString(FlatBufferDeserializationOption option)
-    {
-        SomeStruct s = new SomeStruct
-        {
-            A = 1,
-            B = 2,
-            C = 3,
-            D = 4,
-        };
-
-        SomeStruct parsed = this.ParseStruct(s, option);
-
-        Assert.AreEqual("SomeStruct { A = 1, B = 2, C = 3, D = 4 }", parsed.ToString());
-    }
-
-    [TestMethod]
-    [DynamicData(nameof(DynamicDataHelper.DeserializationModes), typeof(DynamicDataHelper))]
-    public void ValueStruct_ToString(FlatBufferDeserializationOption option)
-    {
-        ValueStruct vs = new ValueStruct
-        {
-            X = 1,
-            Y = 2,
-            Z = 3,
-        };
-
-        ValueStruct parsed = this.ParseStruct(vs, option);
-
-        Assert.AreEqual("ValueStruct { X = 1, Y = 2, Z = 3 }", parsed.ToString());
-    }
-
-    [TestMethod]
-    public void Union_ToString()
-    {
-        var c = this.Setup();
-
-        Assert.AreEqual("Container { Value = [MyUnion { A { X = 1 } }, MyUnion { B { Y = 2 } }, MyUnion { C { Z = 3 } }, MyUnion { D { W = 4 } }], Empty = [] }", c.ToString());
-    }
-
-    private T ParseStruct<T>(T, FlatBufferDeserializationOption option)
-        where T : struct
-    {
-        ISerializer<T> serializer = T.Serializer;
-        int maxBytes = serializer.GetMaxSize(vs);
+        ISerializer<RootTable> serializer = RootTable.Serializer;
+        int maxBytes = serializer.GetMaxSize(table);
         byte[] buffer = new byte[maxBytes];
-        serializer.Write(buffer, vs);
-        int written = serializer.Write(buffer, vs);
-        T parsed = serializer.Parse(buffer.AsMemory().Slice(0, written), option);
-        return parsed;
-    }
-    private Container Setup()
-    {
-        Container c = new Container
-        {
-            Value = new MyUnion[]
-            {
-                new MyUnion(new A()),
-                new MyUnion(new B()),
-                new MyUnion(new C()),
-                new MyUnion(new D()),
-            }
-            Empty = new EmptyUnion[]
-            {
-            }
-        };
+        int written = serializer.Write(buffer, table);
+        RootTable parsed = serializer.Parse(buffer.AsMemory().Slice(0, written), option);
 
-        byte[] buffer = new byte[Container.Serializer.GetMaxSize(c)];
-        Container.Serializer.Write(buffer, c);
-
-        return Container.Serializer.Parse(buffer);
+        Assert.AreEqual("Person { Name = John, Age = 30  }", parsed.Person.ToString());
+        Assert.AreEqual("Empty { }", parsed.EmptyTable.ToString());
+        Assert.AreEqual("Struct { V = 2 }", parsed.Struct.ToString());
+        Assert.AreEqual("ValueStruct { X = 0.5, Y = 1, Z = 0 }", parsed.Vec3.ToString());
+        Assert.AreEqual("Union { [A = { V }, B = { V }, C = { V }, D = { V }] }", parsed.Union.ToString());
+        Assert.AreEqual("EmptyUnion { [] }", parsed.EmptyUnion.ToString());
     }
 
 }
