5d28a03f3185e30121051e030854c7d9e2751739
trumully <truman.mulholland@gmail.com>
2024-05-10 14:48:06 +1200

    test: temporary union string

--- src/FlatSharp.Compiler/SchemaModel/ValueUnionSchemaModel.cs
+++ src/FlatSharp.Compiler/SchemaModel/ValueUnionSchemaModel.cs
@@ -159,7 +159,7 @@ public class ValueUnionSchemaModel : BaseSchemaModel
             writer.AppendLine();
             writer.AppendLine("public byte Discriminator { get; }");
 
-            string itemStrings = string.Join(", ", innerTypes.Select(x => $"{x.resolvedType}"));
+            string itemStrings = string.Join(", ", innerTypes.Select(x => $"{x.value.Key}"));
             string itemStringsWithSpace = this.union.Values.Count == 0 ? " " : $" {itemStrings} ";
             writer.AppendLine($"public override string ToString() => $\"{this.Name} {{{{{itemStringsWithSpace}}}}}\";");
 
